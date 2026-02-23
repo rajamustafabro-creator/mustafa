4c30ff7ffadf02d7796b395c8981787e1574f989
mustafa123-star <rajamustafabro@gmail.com>
2026-02-23 23:59:41 +0500

    C:\Users\Mustafa\Nodestartup\initial\mustafa\program5.js

---
+++ program5.js
@@ -0,0 +1,9 @@
+let sub1 =("Enter marks of subject 1:");
+let sub2 =("Enter marks of subject 2:");
+let sub3=("Enter marks of subject 3:");
+
+let total = sub1 + sub2 + sub3;
+let percentage = total / 3;
+
+console.log("Total =", total);
+console.log("Percentage =", percentage + "%");

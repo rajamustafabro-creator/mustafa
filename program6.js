943e8c8ef58a5f6995f0b7ddefe438f3c9f9c041
mustafa123-star <rajamustafabro@gmail.com>
2026-02-24 00:08:08 +0500

    six upload

---
+++ program6.js
@@ -0,0 +1,7 @@
+const num1 = 10;
+const num2 = 5;
+
+console.log("Addition =", num1 + num2);
+console.log("Subtraction =", num1 - num2);
+console.log("Multiplication =", num1 * num2);
+console.log("Division =", num1 / num2);

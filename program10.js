f5f0d5fcfa88b708c3465372137cd7eb9104f006
mustafa123-star <rajamustafabro@gmail.com>
2026-02-24 00:24:09 +0500

    ten upload

---
+++ program10.js
@@ -0,0 +1,8 @@
+let correctPassword = "1234";
+let enteredPassword = "1234";
+
+if (enteredPassword === correctPassword) {
+    console.log("Access Granted");
+} else {
+    console.log("Wrong Password");
+}

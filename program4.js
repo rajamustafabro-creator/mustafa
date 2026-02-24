9feea12bbd6187b3f8eb13af794454a568776640
rajamustafabro-creator <rajamustafabro@gmail.com>
2026-02-25 00:04:44 +0500

    Update program4.js

--- program4.js
+++ program4.js
@@ -1,7 +1,12 @@
-let age = ("Enter your age:");
-
-if (age >= 18) {
-    console.log("You are an Adult");
-} else {
-    console.log("You are a Minor");
+let agetracker = function(age){
+    return age;
 }
+let age=20
+console.log("youmg person");
+if(agetracker(age) >= 18){
+    console.log("You are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote");
+}
+console.log("Age verification completed");  
